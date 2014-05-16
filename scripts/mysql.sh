9b0ebf3ebc0979b2ac5b6e1975e40dc900f9e324
Florent Viel <florent.viel69@gmail.com>
2014-05-16 14:23:41 +0200

    ajout des scripts d import des timezones

--- scripts/mysql.sh
+++ scripts/mysql.sh
@@ -1,3 +1,5 @@
 echo mysql-server-5.5 mysql-server/root_password password vagrant | debconf-set-selections
 echo mysql-server-5.5 mysql-server/root_password_again password vagrant | debconf-set-selections
 DEBIAN_FRONTEND=noninteractive apt-get -y install mysql-server
+cd /usr/share/zoneinfo && ln -s Europe/Paris CEST
+mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -uroot -pvagrant mysql
