e0418d3508e170050ebabad05bbbeac420f837cc
Florent Viel <florent.viel69@gmail.com>
2014-08-26 10:24:46 +0200

    ajout de php imap

--- scripts/php.sh
+++ scripts/php.sh
@@ -1,4 +1,4 @@
-apt-get -y install php5 php5-cli php5-mysql php5-sqlite php5-intl php5-imagick php5-curl php-apc php5-xdebug php5-memcached php5-xsl php5-gd php5-mcrypt
+apt-get -y install php5 php5-cli php5-mysql php5-sqlite php5-intl php5-imagick php5-curl php-apc php5-xdebug php5-memcached php5-xsl php5-gd php5-mcrypt php5-imap
 echo "date.timezone = Europe/Paris" >> /etc/php5/cli/php.ini
 echo "xdebug.max_nesting_level = 250" >> /etc/php5/conf.d/xdebug.ini
 sed -i 's@memory_limit = 128M@memory_limit = 256M@' /etc/php5/apache2/php.ini
