7ce22b82fff1fc33447c0d504bea99e4f29bcaaa
Florent Viel <florent.viel69@gmail.com>
2014-08-27 18:27:57 +0200

    amélioration de la config php pour cli et apache2

--- scripts/php.sh
+++ scripts/php.sh
@@ -1,7 +1,11 @@
 apt-get -y install php5 php5-cli php5-mysql php5-sqlite php5-intl php5-imagick php5-curl php-apc php5-xdebug php5-memcached php5-xsl php5-gd php5-mcrypt php5-imap php-pear php5-dev
-echo "date.timezone = Europe/Paris" >> /etc/php5/cli/php.ini
 echo "xdebug.max_nesting_level = 250" >> /etc/php5/conf.d/xdebug.ini
+sed -i 's@;date.timezone =@date.timezone = Europe/Paris@' /etc/php5/cli/php.ini
+sed -i 's@;date.timezone =@date.timezone = Europe/Paris@' /etc/php5/apache2/php.ini
+sed -i 's@memory_limit = 128M@memory_limit = 256M@' /etc/php5/cli/php.ini
 sed -i 's@memory_limit = 128M@memory_limit = 256M@' /etc/php5/apache2/php.ini
+sed -i 's@short_open_tag = On@short_open_tag = Off@' /etc/php5/cli/php.ini
+sed -i 's@short_open_tag = On@short_open_tag = Off@' /etc/php5/apache2/php.ini
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 chmod +x /usr/local/bin/composer
