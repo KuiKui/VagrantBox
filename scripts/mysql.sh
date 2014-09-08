e5a4c2883048005c8bd76b46f85988ef22ef1ac0
Florent Viel <florent.viel69@gmail.com>
2014-09-08 20:43:55 +0200

    listen sur 0.0.0.0 et auroriser root sur %

--- scripts/mysql.sh
+++ scripts/mysql.sh
@@ -3,3 +3,5 @@ echo mysql-server-5.5 mysql-server/root_password_again password vagrant | debcon
 DEBIAN_FRONTEND=noninteractive apt-get -y install mysql-server
 cd /usr/share/zoneinfo && ln -s Europe/Paris CEST
 mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -uroot -pvagrant mysql
+sed -i 's@bind-address\t\t= 127.0.0.1@bind-address\t\t= 0.0.0.0@' /etc/mysql/my.cnf
+echo "GRANT ALL ON *.* to root@'%' IDENTIFIED BY 'vagrant';FLUSH PRIVILEGES;" | mysql -uroot -pvagrant
