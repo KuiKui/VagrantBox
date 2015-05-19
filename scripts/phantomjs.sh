dd4484db5f74d1b0def6bf07e4ae37d7b91b11cb
Denis Roussel <denis.roussel@gmail.com>
2015-05-19 17:04:46 +0200

    update phantomjs

--- scripts/phantomjs.sh
+++ scripts/phantomjs.sh
@@ -1,5 +1,17 @@
+# Dependencies
 apt-get -y install fontconfig libfontconfig1 libfreetype6 libstdc++6
-wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-x86_64.tar.bz2
-tar xfj phantomjs-1.9.7-linux-x86_64.tar.bz2 -C /home/vagrant
-chown -R vagrant:vagrant /home/vagrant/phantomjs-1.9.7-linux-x86_64
-echo "export PATH=$PATH:/home/vagrant/phantomjs-1.9.7-linux-x86_64/bin" >> /home/vagrant/.zshrc
+
+# Download
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-linux-x86_64.tar.bz2
+
+# Untar
+tar xfj phantomjs-1.9.8-linux-x86_64.tar.bz2 -C /home/vagrant
+
+# Copy
+cp /home/vagrant/phantomjs-1.9.8-linux-x86_64/bin/phantomjs /usr/local/bin
+
+# Change owner
+chown -R vagrant:vagrant /usr/local/bin/phantomjs
+
+# Clean
+rm -rf /home/vagrant/phantomjs*
