ce82a6b90603553af4ddfbb2112ca475fd9bbe90
Denis Roussel <denis.roussel@gmail.com>
2016-10-12 13:55:53 +0200

    add ldap module for PHP

--- scripts/php.sh
+++ scripts/php.sh
@@ -1,5 +1,5 @@
 # Installation des packets
-apt-get -y install php7.0 libapache2-mod-php7.0 php7.0-mysql php7.0-curl php7.0-json php7.0-gd php7.0-mcrypt php7.0-msgpack php7.0-memcached php7.0-intl php7.0-sqlite3 php7.0-gmp php7.0-geoip php7.0-mbstring php7.0-xml php7.0-zip php7.0-bcmath
+apt-get -y install php7.0 libapache2-mod-php7.0 php7.0-mysql php7.0-curl php7.0-json php7.0-gd php7.0-mcrypt php7.0-msgpack php7.0-memcached php7.0-intl php7.0-sqlite3 php7.0-gmp php7.0-geoip php7.0-mbstring php7.0-xml php7.0-zip php7.0-bcmath php7.0-ldap
 
 # Configuration du module PHP pour apache
 sed -i 's@;date.timezone =@date.timezone = Europe/Paris@' /etc/php/7.0/apache2/php.ini
