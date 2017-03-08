bddcea01e7f045b2f2ce817c87e9ef320b70f17e
Denis Roussel <denis.roussel@gmail.com>
2017-03-08 14:13:34 +0100

    fix php 7.1 configuration

--- scripts/php.sh
+++ scripts/php.sh
@@ -2,11 +2,11 @@
 apt-get -y install php7.1 php7.1-cli libapache2-mod-php7.1 php7.1-mysql php7.1-curl php7.1-json php7.1-gd php7.1-mcrypt php-msgpack php-memcached php7.1-intl php7.1-sqlite3 php7.1-gmp php-geoip php7.1-mbstring php7.1-xml php7.1-zip php7.1-bcmath php7.1-ldap php-redis
 
 # Configuration du module PHP pour apache
-sed -i 's@;date.timezone =@date.timezone = Europe/Paris@' /etc/php/7.0/apache2/php.ini
-sed -i 's@memory_limit = 128M@memory_limit = 256M@' /etc/php/7.0/apache2/php.ini
+sed -i 's@;date.timezone =@date.timezone = Europe/Paris@' /etc/php/7.1/apache2/php.ini
+sed -i 's@memory_limit = 128M@memory_limit = 256M@' /etc/php/7.1/apache2/php.ini
 
 # Configuration de PHP CLI
-sed -i 's@;date.timezone =@date.timezone = Europe/Paris@' /etc/php/7.0/cli/php.ini
+sed -i 's@;date.timezone =@date.timezone = Europe/Paris@' /etc/php/7.1/cli/php.ini
 
 # Installation de Composer
 curl -sS https://getcomposer.org/installer | php
--- scripts/xdebug.sh
+++ scripts/xdebug.sh
@@ -1,8 +1,8 @@
 apt-get install -y php-xdebug
 
-echo "xdebug.var_display_max_depth = 16" | tee -a /etc/php/7.0/mods-available/xdebug.ini
-echo "xdebug.cli_color = 2" | tee -a /etc/php/7.0/mods-available/xdebug.ini
-echo "xdebug.remote_enable = 1" | tee -a /etc/php/7.0/mods-available/xdebug.ini
-echo "xdebug.remote_connect_back = 1" | tee -a /etc/php/7.0/mods-available/xdebug.ini
+echo "xdebug.var_display_max_depth = 16" | tee -a /etc/php/7.1/mods-available/xdebug.ini
+echo "xdebug.cli_color = 2" | tee -a /etc/php/7.1/mods-available/xdebug.ini
+echo "xdebug.remote_enable = 1" | tee -a /etc/php/7.1/mods-available/xdebug.ini
+echo "xdebug.remote_connect_back = 1" | tee -a /etc/php/7.1/mods-available/xdebug.ini
 
 service apache2 restart
