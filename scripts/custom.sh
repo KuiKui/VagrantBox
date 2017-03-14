66411e47c848320fd4205ec2a73cdeca636425b7
Florent Viel <luxifer666@gmail.com>
2017-03-14 10:01:25 +0100

    back to en_US

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -5,9 +5,8 @@ echo "Europe/Paris" > /etc/timezone
 dpkg-reconfigure -f noninteractive tzdata
 
 # Configure locale
-debconf-set-selections <<< 'locales locales/locales_to_be_generated select fr_FR.UTF-8 UTF-8'
-debconf-set-selections <<< 'locales locales/default_environment_locale select fr_FR.UTF-8'
-sed -i 's@# fr_FR.UTF-8@fr_FR.UTF-8@' /etc/locale.gen
+debconf-set-selections <<< 'locales locales/locales_to_be_generated select en_US.UTF-8 UTF-8'
+debconf-set-selections <<< 'locales locales/default_environment_locale select en_US.UTF-8'
 dpkg-reconfigure -f noninteractive locales
-update-locale LANG=fr_FR.UTF-8
-update-locale LC_CTYPE=fr_FR.UTF-8
+update-locale LANG=en_US.UTF-8
+update-locale LC_CTYPE=en_US.UTF-8
