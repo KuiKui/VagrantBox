ef67496ac3770ec9de99b9d96ecf48d9163bf2bc
Denis Roussel <denis.roussel@gmail.com>
2017-05-22 15:43:16 +0200

    add php soap extension

--- scripts/php.sh
+++ scripts/php.sh
@@ -1,5 +1,5 @@
 # Installation des packets
-apt-get -y install php7.1 php7.1-cli libapache2-mod-php7.1 php7.1-mysql php7.1-curl php7.1-json php7.1-gd php7.1-mcrypt php-msgpack php-memcached php7.1-intl php7.1-sqlite3 php7.1-gmp php-geoip php7.1-mbstring php7.1-xml php7.1-zip php7.1-bcmath php7.1-ldap php-redis
+apt-get -y install php7.1 php7.1-cli libapache2-mod-php7.1 php7.1-mysql php7.1-curl php7.1-json php7.1-gd php7.1-mcrypt php-msgpack php-memcached php7.1-intl php7.1-sqlite3 php7.1-gmp php-geoip php7.1-mbstring php7.1-xml php7.1-zip php7.1-bcmath php7.1-ldap php-redis php7.1-soap
 
 # Configuration du module PHP pour apache
 sed -i 's@;date.timezone =@date.timezone = Europe/Paris@' /etc/php/7.1/apache2/php.ini
