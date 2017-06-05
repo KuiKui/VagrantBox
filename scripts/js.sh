b9cef6f2d16f0ad575cbef8da61b391477b6eef8
Hector Jusforgues <hector.jusforgues@gmail.com>
2017-06-05 15:22:21 +0200

    Set correct permissions for /home/vagrant/.config

--- scripts/js.sh
+++ scripts/js.sh
@@ -2,3 +2,4 @@ curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 apt-get -y install nodejs
 curl -L --insecure https://www.npmjs.org/install.sh | bash
 npm install -g gulp
+chown -R vagrant:vagrant /home/vagrant/.config
