b8f8233c5bb93be912634c875e8045f1c6305f96
Florent Viel <luxifer666@gmail.com>
2017-12-01 16:07:04 +0100

    php 7.2

--- scripts/php.sh
+++ scripts/php.sh
@@ -1,12 +1,12 @@
 # Installation des packets
-apt-get -y install php7.1 php7.1-cli libapache2-mod-php7.1 php7.1-mysql php7.1-curl php7.1-json php7.1-gd php7.1-mcrypt php-msgpack php-memcached php7.1-intl php7.1-sqlite3 php7.1-gmp php-geoip php7.1-mbstring php7.1-xml php7.1-zip php7.1-bcmath php7.1-ldap php-redis php7.1-soap
+apt-get -y install php7.2 php7.2-cli libapache2-mod-php7.2 php7.2-mysql php7.2-curl php7.2-json php7.2-gd php7.2-mcrypt php-msgpack php-memcached php7.2-intl php7.2-sqlite3 php7.2-gmp php-geoip php7.2-mbstring php7.2-xml php7.2-zip php7.2-bcmath php7.2-ldap php-redis php7.2-soap
 
 # Configuration du module PHP pour apache
-sed -i 's@;date.timezone =@date.timezone = Europe/Paris@' /etc/php/7.1/apache2/php.ini
-sed -i 's@memory_limit = 128M@memory_limit = 256M@' /etc/php/7.1/apache2/php.ini
+sed -i 's@;date.timezone =@date.timezone = Europe/Paris@' /etc/php/7.2/apache2/php.ini
+sed -i 's@memory_limit = 128M@memory_limit = 256M@' /etc/php/7.2/apache2/php.ini
 
 # Configuration de PHP CLI
-sed -i 's@;date.timezone =@date.timezone = Europe/Paris@' /etc/php/7.1/cli/php.ini
+sed -i 's@;date.timezone =@date.timezone = Europe/Paris@' /etc/php/7.2/cli/php.ini
 
 # Installation de Composer
 curl -sS https://getcomposer.org/installer | php
