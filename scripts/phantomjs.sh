fbae495fc45f6ae0187c47334a301ef74948083b
Florent Viel <luxifer666@gmail.com>
2018-01-19 09:59:21 +0100

    quiet wget

--- scripts/phantomjs.sh
+++ scripts/phantomjs.sh
@@ -2,7 +2,7 @@
 apt-get -y install fontconfig libfontconfig1 libfreetype6 libstdc++6
 
 # Download
-wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+wget -q https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
 
 # Untar
 tar xfj phantomjs-2.1.1-linux-x86_64.tar.bz2
