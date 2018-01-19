b8131b2c1c20205b54eab2cc80c078bd32f6b21a
Florent Viel <luxifer666@gmail.com>
2018-01-19 09:59:58 +0100

    fix composer installation

--- scripts/php.sh
+++ scripts/php.sh
@@ -9,8 +9,19 @@ sed -i 's@memory_limit = 128M@memory_limit = 256M@' /etc/php/7.2/apache2/php.ini
 sed -i 's@;date.timezone =@date.timezone = Europe/Paris@' /etc/php/7.2/cli/php.ini
 
 # Installation de Composer
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
+EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+then
+    >&2 echo 'ERROR: Invalid installer signature'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet --install-dir=/usr/local/bin --filename=composer
+rm composer-setup.php
 chmod +x /usr/local/bin/composer
 composer --version
 mkdir -p /home/vagrant/{.composer,.cache}
