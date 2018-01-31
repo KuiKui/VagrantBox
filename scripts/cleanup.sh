a9e221b6e3b01bdd96ada3e5cdf366cb862783f8
Florent Viel <luxifer666@gmail.com>
2018-01-31 13:46:03 +0100

    after reboot this dir make apt install failed

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -9,7 +9,6 @@ rm /var/lib/dhcp/*
 
 # Make sure Udev doesn't block our network
 echo "cleaning up udev rules"
-mkdir /etc/udev/rules.d/70-persistent-net.rules
 rm -rf /dev/.udev/
 rm /lib/udev/rules.d/75-persistent-net-generator.rules
 
